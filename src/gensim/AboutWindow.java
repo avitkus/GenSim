726f3d47351277ce2be9a0fa4faee4bf44462b51
Andrew Vitkus <avitkus7@gmail.com>
2013-07-18 17:17:24 -0400

    Revised about window and added link to Sourceforge

--- src/gensim/AboutWindow.java
+++ src/gensim/AboutWindow.java
@@ -14,21 +14,22 @@
  * You should have received a copy of the GNU General Public License
  * along with GenSim.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package gensim;
 
 import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Desktop;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Toolkit;
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URISyntaxException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JEditorPane;
@@ -41,110 +42,137 @@ import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
 
 /**
  *
- * @author Andrew
+ * @author Andrew Vitkus
  */
 public class AboutWindow extends JFrame implements Runnable {
-    String bla = "Images lisenced http://creativecommons.org/lisences/by-nc-sa/3.0/";
-    
+
     public AboutWindow() {
         super("About GenSim");
     }
 
     @Override
     public void run() {
-		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         try {
             UIManager.setLookAndFeel(new WindowsLookAndFeel());
         } catch (UnsupportedLookAndFeelException ex) {
         }
         setSize(400, 400);
         buildWindow();
-        
+
+        setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/images/GenSim icon large.png")));
         setLocationByPlatform(true);
         setResizable(false);
         setVisible(true);
     }
-    
+
     private void buildWindow() {
         JTabbedPane tabPane = new JTabbedPane();
-        
-        JPanel mainTab = new JPanel(new GridBagLayout());
-        
-        JLabel versionLabel = new JLabel("GenSim v 1.1");
+
+        JPanel mainTab = new JPanel();
+
+        JEditorPane aboutText = new JEditorPane();
+        try {
+            aboutText.setPage(getClass().getResource("/about.html"));
+        } catch (IOException ex) {
+        }
+        aboutText.setEditable(false);
+        aboutText.setBorder(null);
+        aboutText.setBackground(null);
+        aboutText.addHyperlinkListener(new HyperlinkListener() {
+            @Override
+            public void hyperlinkUpdate(HyperlinkEvent e) {
+                if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+                    try {
+                        Desktop.getDesktop().browse(e.getURL().toURI());
+                    } catch (IOException | URISyntaxException ex) {
+                    }
+                }
+            }
+        });
+        /*
+        JLabel versionLabel = new JLabel("GenSim v 1.2");
         JLabel codedLabel = new JLabel("Coded by: Andrew Vitkus");
         JLabel logoArtLabel = new JLabel("Logo art by: Andrew Vitkus");
         JLabel chickenArtLabel = new JLabel("Chicken art by: Fred Hurteau");
-        JLabel updatedLabel = new JLabel("Last Updated: 6/2013");
+        JLabel updatedLabel = new JLabel("Last Updated: 7/2013");
         JLabel copyrightLabel = new JLabel("Copyright 2013");
-        
+        JLabel websiteLabel = new JLabel("Visit our site to check for updates");
+        JLabel addressLabel = new JLabel("http://sourceforge.net/projects/gen-sim/");
+
         addComponent(mainTab, versionLabel, GridBagConstraints.WEST, 1, 0, 0);
         addComponent(mainTab, codedLabel, GridBagConstraints.WEST, 1, 0, 2);
         addComponent(mainTab, logoArtLabel, GridBagConstraints.WEST, 1, 0, 3);
         addComponent(mainTab, chickenArtLabel, GridBagConstraints.WEST, 1, 0, 4);
         addComponent(mainTab, updatedLabel, GridBagConstraints.WEST, 1, 0, 6);
-        addComponent(mainTab, copyrightLabel, GridBagConstraints.WEST, 1, 0, 8);
-        
+        addComponent(mainTab, copyrightLabel, GridBagConstraints.WEST, 1, 0, 8);*/
+
+        mainTab.add(aboutText);
         JTabbedPane licenseTabs = new JTabbedPane();
-        
+
         JPanel gnugplv3Tab = new JPanel();
         gnugplv3Tab.add(new JLabel("Program licensed under the GNU General Public License version 3"), BorderLayout.NORTH);
-        
-        
+
+
         StringBuilder gplv3Text = new StringBuilder();
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResource("/gpl.txt").openStream(), "ISO-8859-1"))) {
-            while(br.ready()) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResource("/gpl.txt").openStream(), "ISO-8859-1"))) {
+            while (br.ready()) {
                 gplv3Text.append(br.readLine()).append("\n");
             }
-        } catch (IOException ex) { }
-        
+        } catch (IOException ex) {
+        }
+
         JTextArea gplv3 = new JTextArea(gplv3Text.toString());
         gplv3.setEditable(false);
         gplv3.setLineWrap(true);
         gplv3.setWrapStyleWord(true);
         gplv3.setFont(new Font("Serif", Font.PLAIN, 11));
-        
+
         JScrollPane gplv3Scroll = new JScrollPane(gplv3);
         gplv3Scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
         gplv3Scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         gplv3Scroll.setPreferredSize(new Dimension(382, 292));
         gnugplv3Tab.add(gplv3Scroll, BorderLayout.CENTER);
-        
+
         JPanel ccTab = new JPanel();
         ccTab.add(new JLabel("Images licensed under a Creative Commons"), BorderLayout.NORTH);
         ccTab.add(new JLabel("Attribution-NonCommercial-ShareAlike 3.0 Unported License"), BorderLayout.NORTH);
-        
+
         StringBuilder ccText = new StringBuilder();
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResource("/cc-by-nc-sa.txt").openStream(), "ISO-8859-1"))) {
-            while(br.ready()) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResource("/cc-by-nc-sa.txt").openStream(), "ISO-8859-1"))) {
+            while (br.ready()) {
                 ccText.append(br.readLine()).append("\n");
             }
-        } catch (IOException ex) { }
-        
+        } catch (IOException ex) {
+        }
+
         JTextArea cc = new JTextArea(ccText.toString());
-        
+
         cc.setEditable(false);
         cc.setLineWrap(true);
         cc.setWrapStyleWord(true);
         cc.setFont(new Font("Serif", Font.PLAIN, 11));
-        
+
         JScrollPane ccScroll = new JScrollPane(cc);
         ccScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
         ccScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         ccScroll.setPreferredSize(new Dimension(382, 273));
         ccTab.add(ccScroll, BorderLayout.CENTER);
-        
+
         licenseTabs.addTab("Program", null, gnugplv3Tab, "Program license");
         licenseTabs.addTab("Art", null, ccTab, "Art license");
-        
+
         tabPane.addTab("General", null, mainTab, "General information");
         tabPane.addTab("Licenses", null, licenseTabs, "View Licenses");
-        
+
         add(tabPane);
     }
-    
+
     private void addComponent(Container dest, Component component, int fill, int width, int x, int y) {
         GridBagConstraints c = new GridBagConstraints();
 
